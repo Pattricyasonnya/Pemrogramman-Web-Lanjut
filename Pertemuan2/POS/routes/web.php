f64d141da97572e805cec607f1c30677232cab37
Pattricya Sonnya <pattricya13s@gmail.com>
2024-04-01 21:57:26 +0700

    praktikum 3 - js 7

--- Pertemuan2/POS/routes/web.php
+++ Pertemuan2/POS/routes/web.php
@@ -83,3 +83,15 @@ Route::resource('m_user', POSController::class);
 
 //PERTEMUAN 7
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']); //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); //menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); //menampilkan data user baru
+    Route::get('{id}', [UserController::class, 'show']); //menampilkan halaman detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); //menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); //menampilkan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
+
+});
